0180e5ff8204bb7ddb659c43f08de05ea44cf6fc
Jose Luis Bosque <joseluis.bosque@unican.es>
2019-03-14 09:13:53 +0100

    change permissions

--- R/simulate_data.R
+++ R/simulate_data.R
@@ -1,5 +1,5 @@
 #' @title Simulate Data
-#' @description Functoin that simulate complete data, incomplete data and also approximate the top event probability distribution by simulation.
+#' @description Function that simulate complete data, incomplete data and also approximate the top event probability distribution by simulation.
 #' The data is a matrix type in R with the number of rows equal to the number of observation and the number
 #'        of columns equal to the number of nodes in the tree.  The primary nodes must be listed as
 #'        the first columns and the top event node must be the last column.
@@ -180,10 +180,9 @@ simulate_top_event_data <- function(n, p, tree) {
 # Function inputs are: the number of simulations n_simulations to use, and a n x 2 matrix consisting of n beta parameter pairs,
 # one for each of the n primary event probabilities.  Output is a KDE of the prior density of the top event probability
 # ####################################################################################################################################
-simulate_top_event_probability <- function(n_simulations, beta_params, save_figure, figure_filename, x_limits,line_type,tree) {
+simulate_top_event_probability <- function(n_simulations, beta_params, tree, x_limits, lty) {
   x_min <- 0
   x_max <- 1
-
   simulated_p <- matrix(nrow=tree$n_nodes,ncol=n_simulations)
   # Simulate the primary event probabilities from their beta prior
   for (i in 1:tree$n_primary) {
@@ -203,24 +202,16 @@ simulate_top_event_probability <- function(n_simulations, beta_params, save_figu
   # Create an estimate of the density of the top event probability (which we assume is the last-indexed event)
   p_top <- density(simulated_p[tree$n_nodes,],from=x_min,to=x_max)
   plot_y_max <- max(p_top$y)
-  if (save_figure) { pdf(file=figure_filename) }
   par(mfrow=c(1,1))
   plot(p_top$x,p_top$y,type="l",xlab="TOP EVENT PROBABILITY",ylab="DENSITY",main="",
-       xlim=x_limits,ylim=c(0,1.05*plot_y_max),lwd=2,lty=line_type)
+       xlim=x_limits,ylim=c(0,1.05*plot_y_max),lwd=2,lty=lty)
   grid()
-  # x <- seq(from=0,to=1,by=0.01)
-  # lines(x,dbeta(x,shape1=beta_params[1],shape2=beta_params[2]),lty=3)
-  #legend("topright",c("TOP","E5","E6","PRIMARY"),lty=c(1,2,4,3))
-  if (save_figure) { dev.off() }
+  
   cat("Prior probability of top event:","\n")
   cat("   Mean is", mean(simulated_p[tree$n_nodes,]), "\n") # modified for ATV example
   cat("   Standard deviation is", sd(simulated_p[tree$n_nodes,]), "\n")
   cat("   Central 95% probability interval is (",quantile(simulated_p[tree$n_nodes,],0.025),
       ", ",quantile(simulated_p[tree$n_nodes,],0.975),")", "\n",sep="")
-  # par(mfrow=c(1,3))
-  # hist(1 - (1-p1)*(1-p2),xlab="p_5",ylab="Prior Probability",main="",freq=FALSE)
-  # hist(p6,xlab="p_6",ylab="",main="",freq=FALSE)
-  # hist(p_top,xlab="p_TOP",ylab="",main="",freq=FALSE)
 }
 # ###########################################################################################################
 
@@ -267,10 +258,19 @@ or_logic <- function(d, child) {
 # and_scalar. Calculates the probability of a "and" tree node based its children's probabilities
 # #####################################################################################################
 calculate_and_scalar <- function(child, mysim){
+  if (length(dim(mysim))==0) {  # just passing a vector of probabilities
+    just_a_vector <- TRUE 
+  } else {                      # else we're passing a matrix 
+    just_a_vector <- FALSE
+  }
   aux <- 1
   for (j in 1:length(child)) {
     if (child[j]==1) {
-      aux <- aux * mysim[j]
+      if (just_a_vector) {
+         aux <- aux * mysim[j]
+      } else {
+         aux <- aux * mysim[j,]
+      }     
     }
   }
   return (aux)
@@ -280,10 +280,19 @@ calculate_and_scalar <- function(child, mysim){
 # and_scalar. Calculates the probability of a "or" tree node based its children's probabilities.
 # #####################################################################################################
 calculate_or_scalar <- function(child, mysim){
+  if (length(dim(mysim))==0) {  # just passing a vector of probabilities
+    just_a_vector <- TRUE 
+  } else {                      # else we're passing a matrix 
+    just_a_vector <- FALSE
+  }  
   aux <- 1
   for (j in 1:length(child)) {
     if (child[j]==1) {
-      aux <- aux * (1-mysim[j])
+      if (just_a_vector) {
+        aux <- aux * (1-mysim[j])
+      } else {
+        aux <- aux * (1-mysim[j,])
+      }    
     }
   }
   aux <- (1-aux)
--- RiskAssessment_example.R
+++ RiskAssessment_example.R
@@ -1,21 +1,29 @@
-# library("RiskAssessment", lib.loc="~/my_stuff/research/esa/code/Jose_Luis_code")
+# #######################################################################################################################
+# This script takes you through an analysis of the simple example that appears in "Quantitative system risk assessment 
+# from incomplete  data with belief networks and pairwise comparison elicitation" by De Persis et al.
+# Any queries on this code can be sent to Simon Wilson at simon.wilson@tcd.ie
+# #######################################################################################################################
 
 # #######################################################################################################################
 # LOAD IN ALL FUNCTIONS #################################################################################################
-source("RiskAssessment/R/inference.R")
-source("RiskAssessment/R/derive_primary_event_priors.R")
-source("RiskAssessment/R/simulate_data.R")
-source("RiskAssessment/R/tree_definition.R")
+# #######################################################################################################################
+source("./R/inference.R")
+source("./R/derive_primary_event_priors.R")
+source("./R/simulate_data.R")
+source("./R/tree_definition.R")
+source("./R/report_posterior.R")
 # #######################################################################################################################
 
 # #######################################################################################################################
 # LOAD IN THE FAULT TREE STRUCTURE FROM THE SPECIFIED FILE ##############################################################
+# #######################################################################################################################
 # The tree is specified in the file tree.conf and a tree object is created.
-tree <- create_fault_tree("~/my_stuff/research/esa/code/Jose_Luis_code/tree.conf")
+tree <- create_fault_tree("tree.conf")
 # #######################################################################################################################
 
 # #######################################################################################################################
 # DO THE ELICITATION ON THE PRIMARY EVENT PROBABILITIES #################################################################
+# #######################################################################################################################
 # Elicit the beta prior distributions for each primary event probability by the 
 # pairwise comparison approach.  For this we call the derive_primary_event_priors function
 # with arguments:
@@ -28,18 +36,24 @@ tree <- create_fault_tree("~/my_stuff/research/esa/code/Jose_Luis_code/tree.conf
 #   pairwise_matrix: the matrix of pairwise comparisons, as described in the paper. A square matrix
 #                            whose dimension is the number of primary events.  If a particular
 #                            pairwise comparison is not done then the entry is NA
+# Here's a complete pairwise matrix (as used in the paper)
+pairwise_matrix            = matrix(c(1.00, 0.21, 1.04, 0.53,
+                                      1.52, 1.00, 1.04, 1.52,
+                                      0.53, 0.53, 1.00, 0.17,
+                                      1.04, 0.21, 2.55, 1.00), nrow=4,byrow=TRUE)
+# And here's an incomplete one (also used in the paper):
+# pairwise_matrix = matrix(c(1.00, 0.21, 1.04, 0.53,
+#                            1.52, 1.00, NA,   NA,
+#                            0.53, NA,   1.00, NA,
+#                            1.04, NA,   NA,   1.00), nrow=tree$n_primary,byrow=TRUE)
 beta_parameters <- derive_primary_event_priors(cornerstone_event_index    = tree$corner, 
-                                               cornerstone_event_interval = c(0.01,0.05), 
-                                               pairwise_matrix            = matrix(c(1,   1/4,  2, 1/2,
-                                                                                     4, 1,  2, 4,
-                                                                                     1/2, 1/2, 1, 1/5,
-                                                                                     2,   1/4, 5, 1), nrow=4,byrow=TRUE))
-# Or here's an example of an incomplete comparison matrix that you could also use as the pairwise_matrix 
-# argument in the derive_primary_event_priors function:
-# pairwise_matrix = matrix(c(1,   1/4,  2,  1/2,
-#                             4,   1,    NA, NA,
-#                             1/2, NA,   1,  NA,
-#                             2,   NA,   NA, 1), nrow=4,byrow=TRUE)
+                                               cornerstone_event_interval = c(0.01,0.05),
+                                               pairwise_matrix)
+# Use these beta parameters to simulate the top event probability and plot a KDE of its prior.  The other arguments to
+# this function are:
+#   n_simulations: the number of simulations to generate
+#   x_limits: the x-axis limits to use on the plot of the top event prior probability
+simulate_top_event_probability(n_simulations=100000, beta_parameters, tree, x_limits=c(0,0.2))
 # #######################################################################################################################
 
 # #######################################################################################################################
@@ -50,7 +64,8 @@ beta_parameters <- derive_primary_event_priors(cornerstone_event_index    = tree
 #        the first columns and the top event node must be the last column.
 #     Each element of the matrix is either a 1 (that event was observed to occur), 0 (that event
 #        was observed not to occur) or NA (that event was not observed)
-# You can simulate the data as follows: 
+# You can specify data in 3 ways.
+# The first is to simulate the data as follows: 
 data <- simulate_data(n_simulated_data = 5, 
                       tree_definition  = tree, 
                       data_type        = "complete",
@@ -65,11 +80,11 @@ data <- simulate_data(n_simulated_data = 5,
 #     true_primary_p is an array of the true probabilities of each primary event occurring.  Obviously
 #        must be of the same dimension as the number of primary events in tree.
 #     p_obs is, in the case that data_type="incomplete", the probability that a node is observed
-# Or you can load the data from a file (here a csv file).  The file format can be anything really, as long
+# The second is to load the data from a file (here a csv file).  The file format can be anything really, as long
 #    as it is formatted to an R matrix object as decsribed above.  Here we input the data from a CSV file
-data <- as.matrix(read.csv(file="~/my_stuff/research/esa/code/Jose_Luis_code/sample_data.csv",sep=","))
+data <- as.matrix(read.csv(file="sample_data.csv",sep=","))
 colnames(data) <- NULL
-# Or you can just directly enter the matrix of data
+# Thirdly, you can just directly enter the matrix of data
 data <- matrix(c(1,0,0,0,1,0,1,
                  0,0,0,0,0,0,0,
                  0,0,0,0,0,0,0,
@@ -84,23 +99,29 @@ data <- matrix(c(1,0,0,0,1,0,1,
 # burn_in:             initial proportion of samples to discard as burn_in
 # thinning:            save 1 in every thinning samples to use in construction of posterior distributions
 # logit_p_proposal_sd: the std. dev. of the proposal on the logit p_i
-MCMC_parameters <- list(n_MCMC=10000, burn_in=0.2, thinning=10, logit_p_proposal_sd=0.2)
+MCMC_parameters <- list(n_MCMC=100000, burn_in=0.2, thinning=10, logit_p_proposal_sd=0.2)
 # ########################################################################################################################
 
 # ########################################################################################################################
 # COMPUTE POSTERIOR DISTEIBUTION OF EVENT PROBABILITUES BY MCMC AND STORE RESULTS ########################################
 # posterior is a matrix that contains samples from the posterior distribution of the event probabilities.  Each row of
 # the matrix is one sample.  Each column is samples for one of the event probabilities, ordered as specified in tree.
+# If using simulated data, the data_type value must match that used in the simulate_data function
 posterior <- inference(beta_parameters, data, data_type="complete", MCMC_parameters, tree)
 # ########################################################################################################################
 
 # ########################################################################################################################
 # REPORT THE POSTERIOR DISTRIBUTION IN VARIOUS WAYS ######################################################################
+# ########################################################################################################################
+# This includes a text summary for each event probability and 2 plots: one for the top event posterior and one for the 
+# primary event posteriors.  There is an option to include the prior distribution in the top event plot
+# This is still under development and you'll have to go into the code to alter plot parameters such as axes limits!
 report_posterior(posterior, tree,
                  text_summary = TRUE,                   # Print summary posterior statistics to the screen?
                  plot_top_event_posterior = TRUE,       # Plot the top event probability posterior?
-                 plot_primary_event_posterior = TRUE,  # Plot the primary event probabilities posteriors?
-                 include_priors = TRUE,                # Include the prior distribution in these plots?
-                 save_figures = TRUE)                  # Save the figures to file?
+                 plot_primary_event_posterior = FALSE,  # Plot the primary event probabilities posteriors?
+                 include_priors = TRUE,                 # Include the prior distribution in the top event plot?
+                 save_figures = FALSE)                  # Save the 2 figures to file?
+# ########################################################################################################################
 
 
